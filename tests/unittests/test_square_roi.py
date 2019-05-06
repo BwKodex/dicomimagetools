9a1623a52bb3a5602d30db9b6804d1337bd6e2a7
Josef Lundman <josef.lundman@gmail.com>
2019-05-06 22:33:04 +0200

    #10 Add tests for roi size, placement and get_men and get_std methods

--- tests/unittests/test_square_roi.py
+++ tests/unittests/test_square_roi.py
@@ -23,13 +23,6 @@ test_image = np.array(
 )
 
 
-def test_too_small_roi():
-    center = dict(x=6, y=6, z=None)
-    voxel_data = VoxelData(x=10.0, y=10.0, z=None)
-    with pytest.raises(ValueError):
-        SquareRoi(center=center, height=1, width=1, pixel_size=voxel_data)
-
-
 def test_square_roi_creation():
     center = dict(x=6, y=6, z=None)
     voxel_data = VoxelData(x=0.5, y=0.5, z=None)
@@ -49,9 +42,98 @@ def test_square_roi_creation():
     assert square_roi.LowerRight.y == 9
 
 
+def test_too_small_roi():
+    center = dict(x=6, y=6, z=None)
+    voxel_data = VoxelData(x=10.0, y=10.0, z=None)
+    with pytest.raises(ValueError):
+        SquareRoi(center=center, height=1, width=1, pixel_size=voxel_data)
+
+
+def test_upper_left_outside():
+    center = dict(x=1, y=1, z=None)
+    voxel_data = VoxelData(x=1.0, y=1.0, z=None)
+    with pytest.raises(ValueError) as exc_info:
+        SquareRoi(center=center, height=5, width=5, pixel_size=voxel_data)
+    assert 'upper left corner' in str(exc_info.value)
+
+
 def test_unallowed_color():
     center = dict( x=6, y=6, z=None)
     voxel_data = VoxelData(x=1.0, y=1.0, z=None)
     square_roi = SquareRoi(center=center, height=3, width=3, pixel_size=voxel_data)
     with pytest.raises(ValueError):
         square_roi.add_roi_to_image(image=test_image, roi_color="Not a real colour")
+
+
+def test_check_roi_placement_outside():
+    center = dict(x=100, y=100, z=None)
+    voxel_data = VoxelData(x=1.0, y=1.0, z=None)
+    square_roi = SquareRoi(center=center, height=3, width=3, pixel_size=voxel_data)
+    with pytest.raises(ValueError):
+        square_roi._check_roi_placement(image=test_image)
+
+
+def test_check_roi_placement_part_outside():
+    center = dict(x=12, y=12, z=None)
+    voxel_data = VoxelData(x=1.0, y=1.0, z=None)
+    square_roi = SquareRoi(center=center, height=3, width=3, pixel_size=voxel_data)
+    with pytest.raises(ValueError):
+        square_roi._check_roi_placement(image=test_image)
+
+
+def test_get_mean():
+    expected_mean = 7.0
+
+    center = dict(x=6, y=6, z=None)
+    voxel_data = VoxelData(x=1.0, y=1.0, z=None)
+    square_roi = SquareRoi(center=center, height=3, width=3, pixel_size=voxel_data)
+
+    assert expected_mean == square_roi.get_mean(image=test_image)
+
+
+def test_get_mean_too_big_roi_resize():
+    expected_mean = 12.0
+
+    center = dict(x=12, y=12, z=None)
+    voxel_data = VoxelData(x=1.0, y=1.0, z=None)
+    square_roi = SquareRoi(center=center, height=3, width=3, pixel_size=voxel_data, resize_too_big_roi=True)
+
+    assert expected_mean == square_roi.get_mean(image=test_image)
+
+
+def test_get_mean_too_big_roi_no_resize():
+    center = dict(x=12, y=12, z=None)
+    voxel_data = VoxelData(x=1.0, y=1.0, z=None)
+    square_roi = SquareRoi(center=center, height=3, width=3, pixel_size=voxel_data, resize_too_big_roi=False)
+
+    with pytest.raises(ValueError):
+        square_roi.get_mean(image=test_image)
+
+
+def test_get_stdev():
+    expected_stdev = np.std(test_image[5:7 + 1, 5:7 + 1])
+
+    center = dict(x=6, y=6, z=None)
+    voxel_data = VoxelData(x=1.0, y=1.0, z=None)
+    square_roi = SquareRoi(center=center, height=3, width=3, pixel_size=voxel_data)
+
+    assert expected_stdev == square_roi.get_stdev(image=test_image)
+
+
+def test_get_stdev_too_big_roi_resize():
+    expected_mean = 0.5
+
+    center = dict(x=12, y=12, z=None)
+    voxel_data = VoxelData(x=1.0, y=1.0, z=None)
+    square_roi = SquareRoi(center=center, height=5, width=5, pixel_size=voxel_data, resize_too_big_roi=True)
+
+    assert expected_mean == square_roi.get_stdev(image=test_image)
+
+
+def test_get_stdev_too_big_roi_no_resize():
+    center = dict(x=12, y=12, z=None)
+    voxel_data = VoxelData(x=1.0, y=1.0, z=None)
+    square_roi = SquareRoi(center=center, height=3, width=3, pixel_size=voxel_data, resize_too_big_roi=False)
+
+    with pytest.raises(ValueError):
+        square_roi.get_stdev(image=test_image)
