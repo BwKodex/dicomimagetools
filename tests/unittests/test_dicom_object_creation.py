27c46c276949394d5a479f4cd2f7f5e67e30cff9
Josef Lundman <josef.lundman@gmail.com>
2019-05-17 14:06:21 +0200

    #5 Add unittests for DICOM object creation

---
+++ tests/unittests/test_dicom_object_creation.py
@@ -0,0 +1,35 @@
+from dicom_image_tools.dicom_handlers.dicom_study import DicomStudy, DicomSeries
+import pytest
+
+
+def test_dicom_study_creation():
+    expected = dict(
+        type=DicomStudy,
+        study_instance_uid='test-uid'
+    )
+    dcm_obj = DicomStudy(study_instance_uid=expected['study_instance_uid'])
+
+    assert isinstance(dcm_obj, expected.get('type'))
+    assert dcm_obj.StudyInstanceUid == expected['study_instance_uid']
+
+
+def test_dicom_study_creation_wrong_study_instance_uid_type():
+    with pytest.raises(TypeError):
+        dcm_obj = DicomStudy(study_instance_uid=12345)
+
+
+def test_dicom_series_creation():
+    expected = dict(
+        type=DicomSeries,
+        series_instance_uid='test-uid'
+    )
+    dcm_obj = DicomSeries(series_instance_uid=expected['series_instance_uid'])
+
+    assert isinstance(dcm_obj, expected.get('type'))
+    assert dcm_obj.SeriesInstanceUid == expected['series_instance_uid']
+
+
+def test_dicom_study_creation_wrong_series_instance_uid_type():
+    with pytest.raises(TypeError):
+        dcm_obj = DicomSeries(series_instance_uid=12345)
+
