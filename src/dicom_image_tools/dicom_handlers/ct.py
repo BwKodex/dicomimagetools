a538696631c6a533b7e93b80c37a158d9e50b0da
Josef Lundman <josef.lundman@gmail.com>
2019-05-21 09:17:17 +0200

    #6 Add class for CT Series objects

---
+++ src/dicom_image_tools/dicom_handlers/ct.py
@@ -0,0 +1,238 @@
+import logging
+import numpy as np
+from pathlib import Path
+import pydicom
+from pydicom import FileDataset
+from scipy import ndimage
+from scipy.ndimage import center_of_mass
+from skimage import morphology
+from typing import List, Optional
+
+from .dicom_study import DicomSeries
+from ..helpers.pixel_data import get_pixel_array
+from ..helpers.voxel_data import VoxelData
+from ..helpers.patient_centering import PatientMassCenter, PatientGeometricalOffset
+
+
+log = logging.getLogger(__name__)
+
+
+class CtSeries(DicomSeries):
+    """ A class to manage DICOM files from CT connected by a Series Instance UID
+
+    """
+    def __init__(self, series_instance_uid: str):
+        super().__init__(series_instance_uid=series_instance_uid)
+        self.kV: Optional[List[float]] = None
+        self.mA: Optional[List[Optional[float]]] = None
+        self.SlicePosition: Optional[List[float]] = None
+        self.Manufacturer: Optional[str] = None
+
+        # Image volume data
+        self.Mask: Optional[np.ndarray] = None
+        self.MaskSuccess: Optional[bool] = None
+        self.PatientClipped: Optional[bool] = None
+
+        # Calculated image information
+        self.PatientMassCenterImage: Optional[List[PatientMassCenter]] = None
+        self.PatientMassCenterVolume: Optional[PatientMassCenter] = None
+        self.PatientGeometricalOffset: Optional[PatientGeometricalOffset] = None
+        self.MeanHuImage: Optional[List[float]] = None
+        self.MedianHuImage: Optional[List[float]] = None
+        self.MeanHuVolume: Optional[float] = None
+        self.MedianHuVolume: Optional[float] = None
+
+    def add_file(self, file: Path, dcm: Optional[FileDataset] = None) -> None:
+        """ Add a file to the objects list of files.
+
+        First performs a check that the file is a valid DICOM file and that it is a CT file
+
+        Raises:
+            ValueError: If supplied file is not a CT image
+
+        """
+        if dcm is None:
+            dcm = pydicom.dcmread(fp=str(file.absolute()), stop_before_pixels=True)
+
+        if dcm.Modality.upper() != "CT":
+            raise ValueError(f"The supplied file is not a CT image. (supplied modality: {dcm.Modality}")
+
+        super().add_file(file=file, dcm=dcm)
+        self.Manufacturer = dcm.Manufacturer
+
+    def import_image_volume(self) -> None:
+        """ Import the files in the CtVolume and insert them into the ImageVolumeProperty. Also add metadata for each
+        image into the respective property.
+        """
+        # Remove any previously imported image volume
+        self.ImageVolume = None
+
+        for ind, fp in enumerate(self.FilePaths):
+            dcm = pydicom.dcmread(fp=str(fp.absolute()))
+            px = get_pixel_array(dcm=dcm)
+
+            if self.ImageVolume is None:
+                self.ImageVolume = np.empty((px.shape[0], px.shape[1], len(self.FilePaths)))
+
+            self.ImageVolume[:, :, ind] = px
+            self.kV.append(dcm.KVP)
+
+            if 'XRayTubeCurrent' in dcm:
+                self.mA.append(dcm.XRayTubeCurrent)
+            elif 'XRayTubeCurrentInmA' in dcm:
+                self.mA.append(dcm.XRayTubeCurrentInmA)
+            elif 'XRayTubeCurrentInuA' in dcm:
+                self.mA.append(dcm.XRayTubeCurrentInuA / 1000)
+            else:
+                self.mA.append(None)
+
+            self.VoxelData.append(VoxelData(x=float(dcm.PixelSpacing[1]), y=float(dcm.PixelSpacing[0]),
+                                            z=float(dcm.SliceThickness)))
+
+            # Remove pixel data part of dcm
+            try:
+                del dcm[0x7FE00010]
+            except Exception as e:
+                pass
+
+            self.CompleteMetadata.append(dcm)
+
+    def get_patient_mask(self, threshold: Optional[int] = -500, remove_table: Optional[bool] = False):
+        """ Segment the ImageVolume to find the patient/phantom in the images.
+
+        Args:
+            threshold: HU value to use as threshold. Defaults to -500
+            remove_table: Specify if the CT table should be removed from the image. Defaults to False
+
+        Raises:
+            TypeError: If threshold is not an integer
+            ValueError: If there is not image volume to segment
+
+        """
+        if not isinstance(threshold, int):
+            raise TypeError("The threshold must be given as an integer")
+
+        if self.ImageVolume is None:
+            self.import_image_volume()
+            if self.ImageVolume is None:
+                raise ValueError("Found no image volume to segment")
+
+        self.Mask = np.zeros(self.ImageVolume.shape)
+        self.Mask[self.ImageVolume >= threshold] = 1
+
+        if remove_table:
+            # Remove the table by eroding and dilating the image volume
+            if self.Mask.shape[2] > 2:
+                self.Mask = morphology.binary_erosion(image=self.Mask, selem=morphology.cube(width=3))
+            else:
+                for i in range(self.Mask.shape[2]):
+                    self.Mask[:, :, i] = morphology.binary_erosion(image=self.Mask[:, :, i],
+                                                                   selem=morphology.disk(radius=3))
+
+            self.Mask, nb_labels = ndimage.label(self.Mask)
+
+            central_position = [int(np.floor(np.divide(float(self.Mask.shape[0]), 2.0))),
+                                int(np.floor(np.divide(float(self.Mask.shape[1]), 2.0))),
+                                int(np.floor(np.divide(float(self.Mask.shape[2]), 2.0)))]
+
+            central_blob = np.max(self.Mask[(central_position[0] - 2):(central_position[0] + 3),
+                                  (central_position[1] - 2):(central_position[1] + 3),
+                                  central_position[2]])
+
+            self.Mask[self.Mask != central_blob] = 0
+            self.Mask[self.Mask == central_blob] = 1
+
+            if self.Mask.shape[2] > 2:
+                self.Mask = morphology.binary_dilation(image=self.Mask, selem=morphology.cube(width=3))
+            else:
+                for i in range(self.Mask.shape[2]):
+                    self.Mask[:, :, i] = morphology.binary_dilation(image=self.Mask[:, :, i],
+                                                                    selem=morphology.disk(radius=3))
+
+        for i in range(self.Mask.shape[2]):
+            self.Mask[:, :, i] = ndimage.binary_fill_holes(self.Mask[:, :, i]).astype(int)
+
+        if np.sum(self.Mask) > 0:
+            log.info("Axial images segmented successfully")
+            self.MaskSuccess = True
+
+        self.Mask = self.Mask.astype(bool)
+        tmp_mass_center = [center_of_mass(input=self.Mask[:, :, ind]) for ind in range(self.Mask.shape[2])]
+        self.PatientMassCenterImage = [PatientMassCenter(x=obj[1], y=obj[0]) for obj in tmp_mass_center]
+
+        if self.Mask.shape[2] > 1:
+            tmp_mass_center = center_of_mass(input=self.Mask)
+            self.PatientMassCenterVolume = PatientMassCenter(x=tmp_mass_center[1], y=tmp_mass_center[0],
+                                                             z=tmp_mass_center[2])
+        else:
+            self.PatientMassCenterVolume = self.PatientMassCenterImage[0]
+
+        # Try to get the patient geometrical offset
+        try:
+            self._get_patient_geometrical_offset()
+        except Exception as e:
+            log.warning("Could not calculate patient geometrical offset", e)
+
+    def _get_patient_geometrical_offset(self):
+        """ Calculate the patient/phantom geometrical offset from isocenter
+
+        Raises:
+            ValueError: If the DICOM header does not contain all tags required for the calculation
+
+        """
+        log.info("Calculating phantom geometrical offset")
+
+        for ind, mass_center in enumerate(self.PatientMAssCenterImage):
+            if 'ImagePositionPatient' in self.CompleteMetadata[ind]:
+                geometrical_center = (round(mass_center.y, 1), round(mass_center.x, 1))
+                image_position = (float(self.CompleteMetadata[ind].ImagePositionPatient[0]),
+                                  float(self.CompleteMetadata[ind].ImagePositionPatient[1]))
+
+                if self.Manufacturer.upper() in ['SIEMENS', 'PHILIPS']:
+                    # Recalculate image position due to incorrect specifications
+                    image_position[1] += float(self.CompleteMetadata[ind].TableHeight)
+
+                self.PatientGeometricalOffset.append(PatientGeometricalOffset(
+                    x=float(image_position[0]) + geometrical_center[1] + self.VoxelData[ind].x,
+                    y=float(image_position[1]) + geometrical_center[0] + self.VoxelData[ind].y,
+                ))
+                continue
+
+            if 'DataCollectionCenterPatient' in self.CompleteMetadata[ind] and\
+                    'ReconstructionTargetCenterPatient' in self.CompleteMetadata[ind]:
+                log.debug(("Calculating diff from table based on DataCollectionCenterPatient and "
+                           "ReconstructionTargetCenterPatient"))
+                diff_table_x_mm = float(self.CompleteMetadata[ind].ReconstructionTargetCenterPatient[0]) - float(
+                    self.CompleteMetadata[ind].DataCollectionCenterPatient[0])
+                diff_table_y_mm = float(self.CompleteMetadata[ind].ReconstructionTargetCenterPatient[1]) - float(
+                    self.CompleteMetadata[ind].DataCollectionCenterPatient[1])
+
+            elif self.Manufacturer.upper() in ['GE MEDICAL SYSTEMS'] and self.CompleteMetadata[ind][0x431031]:
+                log.debug(f"Calculating diff from table for machine from {self.Manufacturer}")
+                diff_table_x_mm = float(self.CompleteMetadata[ind][0x431031].value[0])
+                diff_table_y_mm = float(self.CompleteMetadata[ind][0x431031].value[1])
+
+            elif self.Manufacturer.upper() in ['TOSHIBA'] and self.CompleteMetadata[ind][0x70051007]:
+                log.debug(f"Calculating diff from table for machine from {self.Manufacturer}")
+                tmp = [float(i) for i in self.CompleteMetadata[ind][0x70051007].value.decode('utf-8').split('\\')]
+                diff_table_x_mm = (tmp[0] - self.CompleteMetadata[ind].Columns / 2.0) * self.VoxelData[ind].x
+                diff_table_y_mm = (tmp[1] - self.CompleteMetadata[ind].Rows / 2.0) * self.VoxelData[ind].y
+
+            else:
+                raise ValueError("The DICOM header does not contain all required tags")
+
+            # Calculate patient offset
+            image_center_x = self.CompleteMetadata[ind].Columns / 2.0
+            image_center_y = self.CompleteMetadata[ind].Rows / 2.0
+
+            log.debug(f"Image center: x={image_center_x}, y={image_center_y}")
+
+            geometrical_center = (round(mass_center[0], 1), round(mass_center[1], 1))
+
+            diff_geom_x_center_mm = (geometrical_center[1] - image_center_x) * self.VoxelData[ind].x
+            diff_geom_y_center_mm = (geometrical_center[0] - image_center_y) * self.VoxelData[ind].y
+
+            self.PatientGeometricalOffset.append(PatientGeometricalOffset(
+                x=diff_table_x_mm - diff_geom_x_center_mm,
+                y=diff_table_y_mm - diff_geom_y_center_mm
+            ))
