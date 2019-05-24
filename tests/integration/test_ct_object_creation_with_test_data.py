9c54588eae26febec355f48272af70709dce9c08
Josef Lundman <josef.lundman@gmail.com>
2019-05-24 12:23:24 +0200

    #6 Add integrations tests for CtSeries

---
+++ tests/integration/test_ct_object_creation_with_test_data.py
@@ -0,0 +1,42 @@
+from pathlib import Path
+import pytest
+
+from dicom_image_tools.dicom_handlers.ct import CtSeries
+
+
+@pytest.fixture()
+def example_data_path_fixture():
+    base_dir = Path(__file__).parent.parent / 'test_data'
+    example_data_paths = {
+        "ct": base_dir / 'ct_study',
+    }
+    return example_data_paths
+
+
+def test_ct_series_add_file(example_data_path_fixture):
+    ct_series = CtSeries(series_instance_uid='1.2.826.0.1.3680043.8.971.31305363770056566540494760179678687617')
+    ct_series.add_file(file=example_data_path_fixture['ct'] / 'serie1' / '1')
+
+    assert len(ct_series.FilePaths) == 1
+
+
+def test_ct_series_import_image_volume(example_data_path_fixture):
+    ct_series = CtSeries(series_instance_uid='1.2.826.0.1.3680043.8.971.31305363770056566540494760179678687617')
+    ct_series.add_file(file=example_data_path_fixture['ct'] / 'serie1' / '1')
+    ct_series.import_image_volume()
+
+    assert len(ct_series.CompleteMetadata) == 1
+    assert ct_series.Manufacturer == 'GE MEDICAL SYSTEMS'
+    assert ct_series.kV == [120]
+    assert ct_series.mA == [520]
+    assert ct_series.SlicePosition == [-7.5]
+    assert ct_series.ImageVolume.shape == (512, 512, 1)
+    
+
+def test_ct_series_pixel_data_removed_from_complete_metadata(example_data_path_fixture):
+    ct_series = CtSeries(series_instance_uid='1.2.826.0.1.3680043.8.971.31305363770056566540494760179678687617')
+    ct_series.add_file(file=example_data_path_fixture['ct'] / 'serie1' / '1')
+    ct_series.import_image_volume()
+
+    with pytest.raises(KeyError):
+        ct_series.CompleteMetadata[0][0x7FE00010]
