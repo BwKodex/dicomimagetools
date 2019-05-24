cf112868b52e5b4fd56a0fe306ef749e3914972c
Josef Lundman <josef.lundman@gmail.com>
2019-05-24 12:04:21 +0200

    #6 Add unittest for CtSeries object creation

---
+++ tests/unit/test_ct_object_creation.py
@@ -0,0 +1,13 @@
+from dicom_image_tools.dicom_handlers.ct import CtSeries
+import pytest
+
+
+def test_ct_series_creation():
+    expected = dict(
+        type=CtSeries,
+        series_instance_uid='SomeSeriesInstanceUid'
+    )
+    ct_series_obj = CtSeries(series_instance_uid=expected.get('series_instance_uid'))
+
+    assert isinstance(ct_series_obj, expected.get('type'))
+    assert ct_series_obj.SeriesInstanceUid == expected.get('series_instance_uid')
