aea8488f571f9369859a78cda68c51c8e979d214
Max Hellström <max.hellstrom@gmail.com>
2020-12-02 19:57:13 +0100

    updated structure of test

--- tests/integration/test_dicom_import.py
+++ tests/integration/test_dicom_import.py
@@ -10,18 +10,17 @@ def test_import_dicom_from_file_should_parse_kv_ma_ms_tags():
 
     file = Path(__file__).parent.parent / 'test_data' / 'io' / 'iotest_60kv_7ma_20ms.dcm'
     
-    expected_output = dict(kV=60.0, mA=7.0, ms=20.0)
-    output = []
-    expected = [True, True, True]
-
+    expected_kv = 60.0
+    expected_ma = 7.0
+    expected_ms = 20.0
+        
     dicom_study = import_dicom_file(file=file)
     dicom_study.Series[0].import_image()
 
-    output.append(dicom_study.Series[0].kV[0] == expected_output['kV'])
-    output.append(dicom_study.Series[0].mA[0] == expected_output['mA'])
-    output.append(dicom_study.Series[0].ms[0] == expected_output['ms'])
+    assert dicom_study.Series[0].kV[0] == expected_kv
+    assert dicom_study.Series[0].mA[0] == expected_ma
+    assert dicom_study.Series[0].ms[0] == expected_ms
 
-    assert output == expected
 
 def test_import_dicom_from_folder_should_find_all_files_in_given_folder():
     expected = 7
