fa82f4546d6c37ad71c525b26a14d3657108afe1
BwKodex <7418760+BwKodex@users.noreply.github.com>
2021-10-09 07:15:26 +0200

    Adds functionality for extracting CT slice order from multiple tags (#20)
    * Adds functionality for extracting CT slice order from ImagePositionPatient and ImageNumer
    
    * Adds test for CT-series XrayTubeCurrent information extraction
    
    * Adds test for test coverage
    
    * Adds test for missing slice location and image position patient tags

--- setup.py
+++ setup.py
@@ -6,11 +6,11 @@ README = (Path(__file__).parent / "README.md").read_text()
 
 setup(
     name="dicom_image_tools",
-    version="21.8.2",
+    version="21.10.1",
     description="Python package for managing DICOM images from different modalities",
     long_description=README,
     long_description_content_type="text/markdown",
-    url="https://dev.azure.com/bwkodex/DicomImageTools",
+    url="https://github.com/BwKodex/dicomimagetools",
     author="Josef Lundman",
     author_email="josef@lundman.eu",
     license="MIT",
@@ -21,6 +21,6 @@ setup(
     ],
     packages=find_packages(where="src"),
     package_dir={"": "src"},
-    install_requires=["pydicom>=2.1.1", "numpy>=1.19.0, <=1.19.2", "scikit-image>=0.17.2", "scipy>=1.5.4"],
+    install_requires=["pydicom>=2.1.1", "numpy>=1.20.0", "scikit-image>=0.17.2", "scipy>=1.5.4"],
     zip_safe=False,
 )
--- tests/integration/test_ct_object_creation_with_test_data.py
+++ tests/integration/test_ct_object_creation_with_test_data.py
@@ -22,6 +22,42 @@ def test_ct_series_add_file(example_data_path_fixture):
     assert len(ct_series.FilePaths) == 1
 
 
+def test_ct_series_add_file_sorts_on_image_position_patient_when_slice_location_missing(example_data_path_fixture):
+    # Arrange
+    expected = "1"
+    ct_series = CtSeries(series_instance_uid="1.2.826.0.1.3680043.8.971.31639893030722307093467532646480365648")
+
+    fp_first_slice = example_data_path_fixture["ct"] / "GE" / "MissingSliceLocationSeries" / "1"
+    fp_last_slice = example_data_path_fixture["ct"] / "GE" / "MissingSliceLocationSeries" / "2"
+
+    # Act
+    ct_series.add_file(file=fp_last_slice)
+    ct_series.add_file(file=fp_first_slice)
+
+    actual = ct_series.FilePaths[0].name
+
+    # Assert
+    assert actual == expected
+
+
+def test_ct_series_add_file_sorts_on_instance_number_when_slice_location_and_image_position_patient_missing(example_data_path_fixture):
+    # Arrange
+    expected = "1"
+    ct_series = CtSeries(series_instance_uid="1.2.826.0.1.3680043.8.971.31639893030722307093467532646480365648")
+
+    fp_first_slice = example_data_path_fixture["ct"] / "GE" / "MissingSliceLocationAndImagePositionPatientSeries" / "1"
+    fp_last_slice = example_data_path_fixture["ct"] / "GE" / "MissingSliceLocationAndImagePositionPatientSeries" / "2"
+
+    # Act
+    ct_series.add_file(file=fp_last_slice)
+    ct_series.add_file(file=fp_first_slice)
+
+    actual = ct_series.FilePaths[0].name
+
+    # Assert
+    assert actual == expected
+
+
 def test_ct_series_raises_error_on_adding_file_from_other_series(example_data_path_fixture):
     ct_series = CtSeries(series_instance_uid="WrongSeriesInstanceUid")
     with pytest.raises(ValueError):
@@ -39,6 +75,25 @@ def test_ct_series_import_image_volume(example_data_path_fixture):
     assert ct_series.mA == [520]
     assert ct_series.SlicePosition == [-7.5]
     assert ct_series.ImageVolume.shape == (512, 512, 1)
+
+
+def test_ct_series_import_image_volume_appends_ma_from_XRayTubeCurrentInmA_and_XRayTubeCurrentInuA_when_XRayTubeCurrent_tag_is_missing(example_data_path_fixture):
+    # Arrange
+    expected = [520.0, 520.0]
+    ct_series = CtSeries(series_instance_uid="1.2.826.0.1.3680043.8.971.31639893030722307093467532646480365648")
+
+    fp_first_slice = example_data_path_fixture["ct"] / "GE" / "MissingSliceLocationSeries" / "1"
+    fp_last_slice = example_data_path_fixture["ct"] / "GE" / "MissingSliceLocationSeries" / "2"
+
+    # Act
+    ct_series.add_file(file=fp_last_slice)
+    ct_series.add_file(file=fp_first_slice)
+    ct_series.import_image_volume()
+
+    actual = ct_series.mA
+
+    # Assert
+    assert actual == expected
 
 
 def test_ct_series_pixel_data_removed_from_complete_metadata(example_data_path_fixture):
@@ -82,3 +137,11 @@ def test_ct_series_get_patient_mask_remove_table(example_data_path_fixture):
     assert ct_series.PatientClipped is False
     assert ct_series.MedianHuPatientVolume == 125.0
     assert floor(ct_series.MeanHuPatientVolume) == 85.0
+
+
+def test_ct_series_get_patient_mask_imports_image_volume_if_not_already_imported(example_data_path_fixture):
+    ct_series = CtSeries(series_instance_uid="1.2.826.0.1.3680043.8.971.31305363770056566540494760179678687617")
+    ct_series.add_file(file=example_data_path_fixture["ct"] / "GE" / "serie1" / "1")
+    ct_series.get_patient_mask(threshold=-500, remove_table=False)
+
+    assert ct_series.MaskSuccess is True
